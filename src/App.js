a90abb8913ce62f79c2f7844888cb459cb82817d
Anna Ermak <ermak-anna86@yandex.ru>
2022-09-19 12:47:06 +0300

    add useState PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import './scss/app.scss'
+import "./scss/app.scss";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
-
+import pizzas from "./assets/pizza.json";
 
 function App() {
   return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-              <PizzaBlock />
-            </div>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((pizza) => (
+              <PizzaBlock
+                key={pizza.id}
+                title={pizza.title}
+                price={pizza.price}
+                imageUrl={pizza.imageUrl}
+                sizes={pizza.sizes}
+                types={pizza.types}
+              />
+            ))}
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
