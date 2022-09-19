3eaf4b6bf69da85c7c6aa0e48673b9ba7052162b
Anna Ermak <ermak-anna86@yandex.ru>
2022-09-19 14:18:59 +0300

    add Popup

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Sort = () => {
-    return (
-        <div className="sort">
-            <div className="sort__label">
-                <svg
-                    width="10"
-                    height="6"
-                    viewBox="0 0 10 6"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                        fill="#2C2C2C"
-                    />
-                </svg>
-                <b>Сортировка по:</b>
-                <span>популярности</span>
-            </div>
-            <div className="sort__popup">
-                <ul>
-                    <li className="active">популярности</li>
-                    <li>цене</li>
-                    <li>алфавиту</li>
-                </ul>
-            </div>
+  const menuList = ["популярности", "цене", "алфавиту"];
+  const [open, setOpen] = useState(false);
+  const [selectMenu, setSelectMenu] = useState(0);
+  const onClickMenu = (index) => {
+    setSelectMenu(index);
+    setOpen(false);
+  };
+  return (
+    <div className="sort">
+      <div className="sort__label">
+        <svg
+          width="10"
+          height="6"
+          viewBox="0 0 10 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+            fill="#2C2C2C"
+          />
+        </svg>
+        <b>Сортировка по:</b>
+        <span onClick={() => setOpen(!open)}>{menuList[selectMenu]}</span>
+      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {menuList.map((el, i) => (
+              <li
+                onClick={() => onClickMenu(i)}
+                key={i}
+                className={selectMenu === i ? "active" : ""}
+              >
+                {el}
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Sort;
