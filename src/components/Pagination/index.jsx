bd59f03f05e6dffae364648e742d3f195ee442f0
Anna Ermak <ermak-anna86@yandex.ru>
2022-09-20 21:04:20 +0300

    add pagination search pizza

---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+import style from "./Paginatuin.module.scss";
+const Pagination = ({ onChangePage }) => {
+  return (
+    <ReactPaginate
+      className={style.root}
+      breakLabel="..."
+      nextLabel=">"
+      onPageChange={(event) => onChangePage(event.selected + 1)}
+      pageRangeDisplayed={4}
+      pageCount={3}
+      previousLabel="<"
+      renderOnZeroPageCount={null}
+    />
+  );
+};
+
+export default Pagination;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,33 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlockSkeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
+import Pagination from "../components/Pagination";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoader, setIsLoader] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pizzas = items
+    .filter((pizza) => {
+      return pizza.title.toLowerCase().includes(searchValue.toLowerCase());
+    })
+    .map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />);
 
   useEffect(() => {
-    fetch("https://632856a8a2e90dab7bddbdbc.mockapi.io/api/v1/items")
+    setIsLoader(true);
+
+    const search = searchValue ? `search=${searchValue}` : "";
+
+    fetch(
+      `https://632856a8a2e90dab7bddbdbc.mockapi.io/api/v1/items?page=${currentPage}&limit=4&${search}`
+    )
       .then((data) => data.json())
       .then((json) => {
         setItems(json);
         setIsLoader(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [searchValue, currentPage]);
   return (
     <>
       <div className="content__top">
@@ -30,8 +43,9 @@ const Home = () => {
           ? [...new Array(6)].map((_, index) => (
               <PizzaBlockSkeleton key={index} />
             ))
-          : items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
+          : pizzas}
       </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </>
   );
 };
